3a37606cfebea7cc214bc85ef691bb15784513f7
Khav Kivar <sebastian.calderon@sansano.usm.cl>
2022-04-04 13:12:53 -0400

    server update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 export default class API{
     static prodServer = "https://vqty0ji4yl.execute-api.us-east-1.amazonaws.com/prod";
     static localServer = "http://localhost:3000";
-
-    static baseURL = API.prodServer;
+    static nginxServer = "http://licman-backend.xyz";
+    static baseURL = API.nginxServer;
 }
 
