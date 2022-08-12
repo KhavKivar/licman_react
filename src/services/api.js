903d09b4d92124ea968c9aeafe0f1b3fa54b20c8
Khav Kivar <sebastian.calderon@sansano.usm.cl>
2022-08-12 16:52:42 -0400

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ export default class API{
   
     static localServer = "http://localhost:3000";
     static nginxServer = "https://licman-backend.xyz";
-    static baseURL = API.localServer;
+    static baseURL = API.nginxServer;
 }
 
